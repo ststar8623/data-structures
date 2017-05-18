02ef95a12c4a49ae9759fa287621f65fa6152dc7
ststar8623 <ststar8623@gmail.com>
2017-05-17 23:06:25 -0700

    tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,44 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = new Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var findTarget = function(children) {
+    for (let i = 0; i < children.length; i++) {
+      if (children[i].value === target) {
+        result = true;
+        return;
+      } else if (children[i].children !== []) {
+        let child = children[i];
+        findTarget(child.children);
+      }
+    }
+  };
+  findTarget(this.children);
+  return result;
 };
 
-
-
+var tree = new Tree(1);
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+console.log(tree.contains(8));
+console.log(tree);
 /*
  * Complexity: What is the time complexity of the above functions?
+ O(n)
  */
