fa6784de16ce0e4324d60b2880111dd041909cd0
ststar8623 <ststar8623@gmail.com>
2017-05-20 16:11:59 -0700

    reimplement linkedList to pseudoclassical

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,59 +1,3 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
-    var node = new Node(value);
-    if (!list.tail) {
-      list.tail = list.head = node;
-    } else {
-      list.tail.next = node;
-      node.previous = list.tail;
-      list.tail = node;
-    }
-  };
-
-  list.addToHead = function(value) {
-    var node = new Node(value);
-    if (!list.head) {
-      return list.addToTail(node);
-    } else {
-      list.head.previous = node;
-      node.next = list.head;
-      list.head = node;
-    }
-  };
-
-  list.removeTail = function() {
-    var current = list.tail;
-    list.tail = list.tail.previous;
-    return current.value;
-  };
-
-  list.removeHead = function() {
-    var current = list.head;
-    list.head = list.head.next;
-    return current.value;
-  };
-
-  list.contains = function(target) {
-    var node = list.head;
-    if (node.value === target) {
-      return true;
-    }
-    while (node.next) {
-      if (node.next.value === target) {
-        return true;
-      } else {
-        node = node.next;
-      }
-    }
-    return false;
-  };
-  return list;
-};
-
 var Node = function(value) {
   var node = {};
   node.value = value;
@@ -61,6 +5,69 @@ var Node = function(value) {
   node.previous = null;
   return node;
 };
+
+var LinkedList = function() {
+  var list = Object.create(LinkedList.prototype);
+  list.head = null;
+  list.tail = null;
+  return list;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  var node = new Node(value);
+  if (!list.tail) {
+    list.tail = list.head = node;
+  } else {
+    list.tail.next = node;
+    node.previous = list.tail;
+    list.tail = node;
+  }
+};
+
+LinkedList.prototype.addToHead = function(value) {
+  var node = new Node(value);
+  if (!list.head) {
+    return list.addToTail(node);
+  } else {
+    list.head.previous = node;
+    node.next = list.head;
+    list.head = node;
+  }
+};
+
+LinkedList.prototype.removeTail = function() {
+  var current = list.tail;
+  list.tail = list.tail.previous;
+  return current.value;
+};
+
+LinkedList.prototype.removeHead = function() {
+  var current = list.head;
+  list.head = list.head.next;
+  return current.value;
+};
+
+LinkedList.prototype.contains = function(target) {
+  var node = list.head;
+  if (node.value === target) {
+    return true;
+  }
+  while (node.next) {
+    if (node.next.value === target) {
+      return true;
+    } else {
+      node = node.next;
+    }
+  }
+  return false;
+};
+
+var list = new LinkedList();
+list.addToTail(8);
+list.addToHead(1);
+list.addToTail(7);
+
+console.log(list);
 /*
  * Complexity: What is the time complexity of the above functions?
  O(n)
